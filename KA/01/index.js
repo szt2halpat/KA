d5028168cfe4c8f51f31bcfb5879c38eb6b8eb49
szt2halpat <htgaming0670@gmail.com>
2023-10-05 19:10:31 +0200

    Törlés

--- KA/01/index.js
+++
@@ -1,7 +0,0 @@
-const themeToggle = document.getElementById("theme-toggle");
-const body = document.body;
-
-themeToggle.addEventListener("click", () => {
-    body.classList.toggle("light");
-    body.classList.toggle("dark");
-});
