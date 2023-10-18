d47c5ea3fd892b360b706ecc43b47a3b68f21d18
Fricosz <halaszpatrik1234@gmail.com>
2023-10-18 13:24:24 +0200

    Objektumok

--- Web/Objektum/objektum.js
+++ Web/Objektum/objektum.js
@@ -1,3 +1,9 @@
+window.onload = (event) => {
+  megjelenites();
+};
+
+//1. rész
+
 const user = { firstname: "John", lastname: "Smith" };
 
 //Módosítás
@@ -58,3 +64,23 @@ const fiz = document.getElementById("fizetes");
 fiz.innerText = "Ennyit kerestek: " + osszeg + " Ft";
 
 console.log(osszeg);
+
+//4. rész
+
+const heroes = [
+  { firstName: "Ahsoka", lastName: "Tano", job: "padawan" },
+  { firstName: "Boba", lastName: "Fett", job: "fejvadász" },
+  { firstName: "Han", lastName: "Solo", job: "csempész" },
+  { firstName: "Leia", lastName: "Organa", job: "szenátor" },
+  { firstName: "Teszt", lastName: "Teszt", job: "Teszt" },
+];
+
+const tag = document.getElementById("negyedik");
+
+let content = ``;
+
+function megjelenites() {
+  heroes.forEach((element) => {
+    tag.innerHTML += `<div id="container"><div class="card">${element.firstName}</div> <div class="card">${element.lastName}</div> <div class="card">${element.job}</div></div>`;
+  });
+}
